4fe04f1b99c92e15d69f38b23c0f63d52ee9ab84
Prince Billy Graham Karmoker <princebillygrahamkarmoker@gmail.com>
2019-06-06 15:23:41 +0600

    remove dist

--- dist/stat_machine.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	include 'config/init.php'; 
-	$db = new Database();
-
-	$db->query('SELECT * FROM `switchview`');
-	$switchs=$db->fetchAll();
-	foreach ($switchs as $key => $switch)
-		echo $switch->code.'='.$switch->value.'<br>';
-?>
