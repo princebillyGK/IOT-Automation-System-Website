07263b7fd1cb9f3d747eaa65008c974b72f3ddd9
princebillyGK <princebillygrahamkarmoker@gmail.com>
2019-06-30 20:44:01 +0600

    configuring repository

--- src/config/config.php
+++ src/config/config.php
@@ -1,12 +1,13 @@
 <!-- config.php -->
 <?php 
 	//Database Configuration
-	define('DB_HOST','sql300.epizy.com');
-	define('DB_USER','epiz_24068767');
-	define('DB_PASS','GT4MbU1w0Fh2');
-	define('DB_NAME','epiz_24068767_iot_cn');
+	define('DB_HOST','localhost');
+	define('DB_USER','root');
+	define('DB_PASS','');
+	define('DB_NAME','iot_cn');
 
 	#Site Configuration
-	define('SITE_TITLE','Home Automation System');
-	define('SITE_CODE', 'HAS');
+	define('SITE_TITLE','IOT Automation System');
+	define('SITE_CODE', 'IOTAS');
  ?>
+
