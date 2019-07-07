7de1ddec66a7aa07ba66af45f7bf5d164c0335d9
princebillyGK <princebillygrahamkarmoker@gmail.com>
2019-07-08 00:43:28 +0600

    final check

---
+++ src/config/config(server).php
@@ -0,0 +1,13 @@
+<!-- config.php -->
+<?php 
+	//Database Configuration
+	define('DB_HOST','localhost');
+	define('DB_USER','kitsskif_princebillyGK');
+	define('DB_PASS','princebillyGK1998');
+	define('DB_NAME','kitsskif_demo');
+
+	#Site Configuration
+	define('SITE_TITLE','IOT Automation System');
+	define('SITE_CODE', 'IOTAS');
+ ?>
+
